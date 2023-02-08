cc1cb78941534b1803e99237e720e2213cba625b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-02-08 16:25:39 +0100

    updated 20230208

--- src/f_main.pas
+++ src/f_main.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  ActnList, StdActns, Menus, ExtDlgs;
+  ActnList, StdActns, Menus, ExtDlgs, Vcl.ExtCtrls, Vcl.Imaging.jpeg,
+  Vcl.Imaging.pngimage;
 
 type
   Tfrm_main = class(TForm)
@@ -55,100 +56,113 @@ var
 
 implementation
 
-uses fileCtrl, u_param, f_operation_en_cours, f_apropos, f_album_info, f_photo_ajout,
+uses fileCtrl, u_param, f_operation_en_cours, f_apropos, f_album_info,
+  f_photo_ajout,
   f_photo_modif, f_photo_supprimer, f_photo_trier, u_generation,
-  u_chgt_version;
+  u_chgt_version, u_urlOpen;
 
 {$R *.DFM}
 
 procedure Tfrm_main.Nouveau1Click(Sender: TObject);
 var
-   prog_folder : string;
-   album_folder : string;
+  prog_folder: string;
+  album_folder: string;
 begin
-  prog_folder := ExtractFilePath (Application.ExeName);
-  if (Album_Ouvert)
-  then
-    if (mrYes = MessageDlg ('Un album est actuellement ouvert, voulez-vous le fermer ?', mtConfirmation, [mbYes, mbNo], 0))
-    then
-      Fermer1Click (Sender)
+  prog_folder := ExtractFilePath(Application.ExeName);
+  if (Album_Ouvert) then
+    if (mrYes = MessageDlg
+      ('Un album est actuellement ouvert, voulez-vous le fermer ?',
+      mtConfirmation, [mbYes, mbNo], 0)) then
+      Fermer1Click(Sender)
     else
       exit;
-    {endif}
-  {endif}
-  if (NouvelAlbum.InitialDir = '')
-  then
-    NouvelAlbum.InitialDir := prog_folder+'album';
-  {endif}
-  if (NouvelAlbum.Execute)
-  then
-    if (DirectoryExists (NouvelAlbum.FileName))
-    then
-      MessageDlg ('Le dossier '+ExtractFileName (NouvelAlbum.FileName)+' existe déjà. Création de l''album impossible', mtError, [mbCancel], 0)
+  { endif }
+  { endif }
+  if (NouvelAlbum.InitialDir = '') then
+    NouvelAlbum.InitialDir := prog_folder + 'album';
+  { endif }
+  if (NouvelAlbum.Execute) then
+    if (DirectoryExists(NouvelAlbum.FileName)) then
+      MessageDlg('Le dossier ' + ExtractFileName(NouvelAlbum.FileName) +
+        ' existe déjà. Création de l''album impossible', mtError, [mbCancel], 0)
     else
-      begin
-        oec_Ouverture (20);
-        album_folder := NouvelAlbum.FileName;
-        oec_Operation_Suivante;
-        ForceDirectories (album_folder);
-        oec_Operation_Suivante;
-        ForceDirectories (album_folder+'\comment');
-        oec_Operation_Suivante;
-        ForceDirectories (album_folder+'\img');
-        oec_Operation_Suivante;
-        ForceDirectories (album_folder+'\btn');
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\autorun.inf'), PChar (album_folder+'\autorun.inf'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\shelexec.exe'), PChar (album_folder+'\shelexec.exe'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\pag.ico'), PChar (album_folder+'\pag.ico'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\barre.gif'), PChar (album_folder+'\btn\barre.gif'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\haut.gif'), PChar (album_folder+'\btn\haut.gif'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\gauche.gif'), PChar (album_folder+'\btn\gauche.gif'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'ressources\droite.gif'), PChar (album_folder+'\btn\droite.gif'), False);
-        oec_Operation_Suivante;
-        ForceDirectories (album_folder+'\mdl');
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'mdl\browser.mdl'), PChar (album_folder+'\mdl\browser.mdl'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'mdl\credit.mdl'), PChar (album_folder+'\mdl\credit.mdl'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'mdl\index.mdl'), PChar (album_folder+'\mdl\index.mdl'), False);
-        oec_Operation_Suivante;
-        CopyFile (PChar (Prog_Folder+'mdl\photo.mdl'), PChar (album_folder+'\mdl\photo.mdl'), False);
-        oec_Operation_Suivante;
-        Param_Ouvrir_Album (album_folder+'\'+ExtractFileName (album_folder)+'.pag');
-        oec_Operation_Suivante;
-        Ecrire_Version_Courante;
-        oec_Operation_Suivante;
-        oec_Fermeture;
-        N1Informationsgnrales1Click (Sender);
-        Ajouter1Click (Sender);
-        N3Gnrerlalbum1Click (Sender);
-        N4Testerlalbum1Click (Sender);
-        Fermer1.Enabled := True;
-        Album1.Enabled := True;
-        Modifier1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-        Supprimer1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-      end;
-    {endif}
-  {endif}
+    begin
+      oec_Ouverture(20);
+      album_folder := NouvelAlbum.FileName;
+      oec_Operation_Suivante;
+      ForceDirectories(album_folder);
+      oec_Operation_Suivante;
+      ForceDirectories(album_folder + '\comment');
+      oec_Operation_Suivante;
+      ForceDirectories(album_folder + '\img');
+      oec_Operation_Suivante;
+      ForceDirectories(album_folder + '\btn');
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\autorun.inf'),
+        PChar(album_folder + '\autorun.inf'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\shelexec.exe'),
+        PChar(album_folder + '\shelexec.exe'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\pag.ico'),
+        PChar(album_folder + '\pag.ico'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\barre.gif'),
+        PChar(album_folder + '\btn\barre.gif'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\haut.gif'),
+        PChar(album_folder + '\btn\haut.gif'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\gauche.gif'),
+        PChar(album_folder + '\btn\gauche.gif'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'ressources\droite.gif'),
+        PChar(album_folder + '\btn\droite.gif'), False);
+      oec_Operation_Suivante;
+      ForceDirectories(album_folder + '\mdl');
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'mdl\browser.mdl'),
+        PChar(album_folder + '\mdl\browser.mdl'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'mdl\credit.mdl'),
+        PChar(album_folder + '\mdl\credit.mdl'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'mdl\index.mdl'),
+        PChar(album_folder + '\mdl\index.mdl'), False);
+      oec_Operation_Suivante;
+      CopyFile(PChar(prog_folder + 'mdl\photo.mdl'),
+        PChar(album_folder + '\mdl\photo.mdl'), False);
+      oec_Operation_Suivante;
+      Param_Ouvrir_Album(album_folder + '\' + ExtractFileName(album_folder)
+        + '.pag');
+      oec_Operation_Suivante;
+      Ecrire_Version_Courante;
+      oec_Operation_Suivante;
+      oec_Fermeture;
+      N1Informationsgnrales1Click(Sender);
+      Ajouter1Click(Sender);
+      N3Gnrerlalbum1Click(Sender);
+      N4Testerlalbum1Click(Sender);
+      Fermer1.Enabled := True;
+      Album1.Enabled := True;
+      Modifier1.Enabled := Param_Album.ReadInteger('photos',
+        'nb_photos', 0) > 0;
+      Supprimer1.Enabled := Param_Album.ReadInteger('photos',
+        'nb_photos', 0) > 0;
+    end;
+  { endif }
+  { endif }
 end;
 
 procedure Tfrm_main.Fermer1Click(Sender: TObject);
 begin
-  if (Album_Modifie)
-  then
-    if (mrYes = MessageDlg ('Cet album a été modifié mais pas généré, voulez-vous le générer avant de le fermer ?', mtConfirmation, [mbYes, mbNo], 0))
-    then
-      N3Gnrerlalbum1Click (Sender);
-    {endif}
-  {endif}
+  if (Album_Modifie) then
+    if (mrYes = MessageDlg
+      ('Cet album a été modifié mais pas généré, voulez-vous le générer avant de le fermer ?',
+      mtConfirmation, [mbYes, mbNo], 0)) then
+      N3Gnrerlalbum1Click(Sender);
+  { endif }
+  { endif }
   Param_Fermer_Album;
   Fermer1.Enabled := False;
   Album1.Enabled := False;
@@ -156,110 +170,108 @@ end;
 
 procedure Tfrm_main.N1Informationsgnrales1Click(Sender: TObject);
 var
-   f : TFrm_album_info;
+  f: TFrm_album_info;
 begin
-  f := TFrm_Album_Info.Create (Self);
+  f := TFrm_album_info.Create(Self);
   try
     f.ShowModal;
   finally
-    F.Free;
+    f.Free;
   end;
 end;
 
 procedure Tfrm_main.Ajouter1Click(Sender: TObject);
 var
-   f : TFrm_Photo_Ajout;
-   i : integer;
+  f: TFrm_Photo_Ajout;
+  i: integer;
 begin
-  f := TFrm_Photo_Ajout.Create (Self);
+  f := TFrm_Photo_Ajout.Create(Self);
   try
-    if (OuvrirJPG.InitialDir = '')
-    then
-      OuvrirJPG.InitialDIR := Param_Album.ReadString ('photos', 'default_img_path', Album_Path);
-    {endif}
-    if (OuvrirJPG.Execute)
-    then
-      for i := 0 to Pred (OuvrirJPG.Files.Count) do
-        begin
-          if (i = 0)
-          then
-            Param_Album.WriteString ('photos', 'default_img_path', ExtractFilePath (OuvrirJPG.Files [i]));
-          {endif}
-          f.edt_NomPhoto.Text := OuvrirJPG.Files [i];
-          f.edt_Commentaire.Lines.Clear;
-          f.ShowModal;
-        end;
-      {endfor}
-    {endif}
+    if (OuvrirJPG.InitialDir = '') then
+      OuvrirJPG.InitialDir := Param_Album.ReadString('photos',
+        'default_img_path', Album_Path);
+    { endif }
+    if (OuvrirJPG.Execute) then
+      for i := 0 to Pred(OuvrirJPG.Files.Count) do
+      begin
+        if (i = 0) then
+          Param_Album.WriteString('photos', 'default_img_path',
+            ExtractFilePath(OuvrirJPG.Files[i]));
+        { endif }
+        f.edt_NomPhoto.Text := OuvrirJPG.Files[i];
+        f.edt_Commentaire.Lines.Clear;
+        f.ShowModal;
+      end;
+    { endfor }
+    { endif }
   finally
     f.Free;
   end;
-  Modifier1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-  Supprimer1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
+  Modifier1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
+  Supprimer1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
   SetFocus;
 end;
 
 procedure Tfrm_main.N3Gnrerlalbum1Click(Sender: TObject);
 var
-   i : integer;
+  i: integer;
 begin
-  oec_Ouverture (8+Param_Album.ReadInteger ('photos', 'nb_photos', 0));
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'index.htm', 0);
+  oec_Ouverture(8 + Param_Album.ReadInteger('photos', 'nb_photos', 0));
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'index.htm', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'default.htm', 0);
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'default.htm', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'homepage.htm', 0);
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'homepage.htm', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'index.html', 0);
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'index.html', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'default.html', 0);
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'default.html', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\index.mdl', Album_Path+'homepage.html', 0);
+  Generer_Page(Album_Path + 'mdl\index.mdl', Album_Path + 'homepage.html', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\browser.mdl', Album_Path+'browser.html', 0);
+  Generer_Page(Album_Path + 'mdl\browser.mdl', Album_Path + 'browser.html', 0);
   oec_Operation_Suivante;
-  Generer_Page (Album_Path+'mdl\credit.mdl', Album_Path+'credit.html', 0);
+  Generer_Page(Album_Path + 'mdl\credit.mdl', Album_Path + 'credit.html', 0);
   oec_Operation_Suivante;
-  for i := 1 to Param_Album.ReadInteger ('photos', 'nb_photos', 0) do
-    begin
-      Generer_Page (Album_Path+'mdl\photo.mdl', Album_Path+'page'+IntToStr (i)+'.html', i);
-      oec_Operation_Suivante;
-    end;
-  {endfor}
+  for i := 1 to Param_Album.ReadInteger('photos', 'nb_photos', 0) do
+  begin
+    Generer_Page(Album_Path + 'mdl\photo.mdl', Album_Path + 'page' + IntToStr(i)
+      + '.html', i);
+    oec_Operation_Suivante;
+  end;
+  { endfor }
   oec_Fermeture;
-  MessageDlg ('Album photo généré', mtInformation, [mbOK], 0);
+  MessageDlg('Album photo généré', mtInformation, [mbOK], 0);
   Album_Modifie := False;
 end;
 
 procedure Tfrm_main.Ouvrir1Click(Sender: TObject);
 var
-   prog_folder : string;
+  prog_folder: string;
 begin
-  prog_folder := ExtractFilePath (Application.ExeName);
-  if (Album_Ouvert)
-  then
-    if (mrYes = MessageDlg ('Un album est actuellement ouvert, voulez-vous le fermer ?', mtConfirmation, [mbYes, mbNo], 0))
-    then
-      Fermer1Click (Sender)
+  prog_folder := ExtractFilePath(Application.ExeName);
+  if (Album_Ouvert) then
+    if (mrYes = MessageDlg
+      ('Un album est actuellement ouvert, voulez-vous le fermer ?',
+      mtConfirmation, [mbYes, mbNo], 0)) then
+      Fermer1Click(Sender)
     else
-      Exit;
-    {endif}
-  {endif}
-  if (OuvrirPAG.InitialDir = '')
-  then
-    OuvrirPAG.InitialDir := prog_folder+'album';
-  {endif}
-  if (OuvrirPAG.Execute)
-  then
-    begin
-      Param_Ouvrir_Album (OuvrirPAG.FileName);
-      Fermer1.Enabled := True;
-      Album1.Enabled := True;
-      Modifier1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-      Supprimer1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-      Mettre_A_Niveau_Album;
-    end;
-  {endif}
+      exit;
+  { endif }
+  { endif }
+  if (OuvrirPAG.InitialDir = '') then
+    OuvrirPAG.InitialDir := prog_folder + 'album';
+  { endif }
+  if (OuvrirPAG.Execute) then
+  begin
+    Param_Ouvrir_Album(OuvrirPAG.FileName);
+    Fermer1.Enabled := True;
+    Album1.Enabled := True;
+    Modifier1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
+    Supprimer1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
+    Mettre_A_Niveau_Album;
+  end;
+  { endif }
 end;
 
 procedure Tfrm_main.FormCreate(Sender: TObject);
@@ -277,9 +289,9 @@ end;
 
 procedure Tfrm_main.Apropos1Click(Sender: TObject);
 var
-   f : TFrm_APropos;
+  f: TFrm_APropos;
 begin
-  f := TFrm_APropos.Create (Self);
+  f := TFrm_APropos.Create(Self);
   try
     f.ShowModal;
   finally
@@ -289,17 +301,16 @@ end;
 
 procedure Tfrm_main.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-  if (Album_Ouvert)
-  then
-    Fermer1Click (Sender);
-  {endif}
+  if (Album_Ouvert) then
+    Fermer1Click(Sender);
+  { endif }
 end;
 
 procedure Tfrm_main.Modifier1Click(Sender: TObject);
 var
-   f : TFrm_Photo_Modif;
+  f: TFrm_Photo_Modif;
 begin
-  f := TFrm_Photo_Modif.Create (Self);
+  f := TFrm_Photo_Modif.Create(Self);
   try
     f.ShowModal;
   finally
@@ -310,24 +321,24 @@ end;
 
 procedure Tfrm_main.Supprimer1Click(Sender: TObject);
 var
-   f : TFrm_Photo_Supprimer;
+  f: TFrm_Photo_Supprimer;
 begin
-  f := TFrm_Photo_Supprimer.Create (Self);
+  f := TFrm_Photo_Supprimer.Create(Self);
   try
     f.ShowModal;
   finally
     f.Free;
   end;
-  Modifier1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
-  Supprimer1.Enabled := Param_Album.ReadInteger ('photos', 'nb_photos', 0) > 0;
+  Modifier1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
+  Supprimer1.Enabled := Param_Album.ReadInteger('photos', 'nb_photos', 0) > 0;
   SetFocus;
 end;
 
 procedure Tfrm_main.Trier1Click(Sender: TObject);
 var
-   f : TFrm_Photo_Trier;
+  f: TFrm_Photo_Trier;
 begin
-  f := TFrm_Photo_Trier.Create (Self);
+  f := TFrm_Photo_Trier.Create(Self);
   try
     f.ShowModal;
   finally
@@ -338,7 +349,7 @@ end;
 
 procedure Tfrm_main.N4Testerlalbum1Click(Sender: TObject);
 begin
-  WinExec (PChar (ExtractFilePath (Application.ExeName)+'ressources\shelexec '+album_path+'index.html'), sw_normal);
+  url_Open_In_Browser(Album_Path + 'index.html');
 end;
 
 end.
--- src/f_photo_detail.pas
+++ src/f_photo_detail.pas
@@ -18,7 +18,7 @@ type
 var
   frm_photo_detail: Tfrm_photo_detail;
 
-procedure Afficher_Photo (Sender: TForm; Nom: string);
+procedure Afficher_Photo(Sender: TForm; Nom: string);
 
 procedure Cacher_Photo;
 
@@ -32,96 +32,94 @@ implementation
 {$R *.DFM}
 
 uses
-    jpeg;
+  jpeg;
 
 var
-   f : TFrm_Photo_Detail;
+  f: Tfrm_photo_detail;
 
-procedure Afficher_Photo (Sender: TForm; Nom: string);
+procedure Afficher_Photo(Sender: TForm; Nom: string);
 begin
-  if (f = nil)
-  then
-    f := TFrm_Photo_Detail.Create (nil);
-  {endif}
+  if (f = nil) then
+    f := Tfrm_photo_detail.Create(nil);
+  { endif }
   try
     f.Hide;
     f.Autosize := False;
-    f.Image1.Picture.LoadFromFile (Nom);
+    f.Image1.Picture.LoadFromFile(Nom);
     f.Autosize := True;
     f.Show;
   finally
   end;
-  if (Sender <> nil)
-  then
+  if (Sender <> nil) then
     Sender.SetFocus;
-  {endif}
+  { endif }
 end;
 
 procedure Cacher_Photo;
 begin
-  if (f <> nil)
-  then
+  if (f <> nil) then
     f.Hide;
-  {endif}
+  { endif }
 end;
 
 function Get_Photo_Larg: Integer;
 begin
-  if (f.Image1.Picture.Width > f.Image1.Picture.Height)
-  then
-    if (f.Image1.Picture.Width > 600)
-    then
+  if (f.Image1.Picture.Width = f.Image1.Picture.Height) then
+    result := f.Image1.Picture.Width
+  else if (f.Image1.Picture.Width > f.Image1.Picture.Height) then
+    if (f.Image1.Picture.Width > 600) then
       result := 600
     else
       result := f.Image1.Picture.Width
-    {endif}
+      { endif }
   else
-    result := (f.Image1.Picture.Width * Get_Photo_Haut) div f.Image1.Picture.Height
-  {endif}
+    result := (f.Image1.Picture.Width * Get_Photo_Haut)
+      div f.Image1.Picture.Height
+    { endif }
 end;
 
 function Get_Photo_Haut: Integer;
 begin
-  if (f.Image1.Picture.Height > f.Image1.Picture.Width)
-  then
-    if (f.Image1.Picture.Height > 600)
-    then
+  if (f.Image1.Picture.Height = f.Image1.Picture.Width) then
+    result := f.Image1.Picture.Height
+  else if (f.Image1.Picture.Height > f.Image1.Picture.Width) then
+    if (f.Image1.Picture.Height > 600) then
       result := 600
     else
       result := f.Image1.Picture.Height
-    {endif}
+      { endif }
   else
-    result := (f.Image1.Picture.Height * Get_Photo_Larg) div f.Image1.Picture.Width
-  {endif}
+    result := (f.Image1.Picture.Height * Get_Photo_Larg)
+      div f.Image1.Picture.Width
+    { endif }
 end;
 
 function Get_Photo_Larg_Red: Integer;
 begin
-  if (f.Image1.Picture.Width > f.Image1.Picture.Height)
-  then
+  if (f.Image1.Picture.Width > f.Image1.Picture.Height) then
     result := 100
   else
     result := (f.Image1.Picture.Width * 100) div f.Image1.Picture.Height;
-  {endif}
+  { endif }
 end;
 
 function Get_Photo_Haut_Red: Integer;
 begin
-  if (f.Image1.Picture.Width < f.Image1.Picture.Height)
-  then
+  if (f.Image1.Picture.Width < f.Image1.Picture.Height) then
     result := 100
   else
     result := (f.Image1.Picture.Height * 100) div f.Image1.Picture.Width;
-  {endif}
+  { endif }
 end;
 
 initialization
-  f := nil;
+
+f := nil;
 
 finalization
-  if (f <> nil)
-  then
-    f.Free;
-  {endif}
+
+if (f <> nil) then
+  f.Free;
+{ endif }
 
 end.
